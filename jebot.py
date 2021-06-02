66ab1e36068bc247f04a3dec368fbad8fe3ce747
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-06-02 09:58:29 +0700

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -77,20 +77,20 @@ PERINTAH UNTUK SEMUA ANGGOTA GRUP
 
 × /playlist : Untuk Menampilkan daftar putar Lagu sekarang
  × /current : Untuk Menunjukkan  Lagu sekarang yang sedang diputar
- × /song <judul lagu> : Untuk Mendownload lagu di YouTube 
- × /video <judul lagu> : Untuk Mendownload Video di YouTube dengan detail
- × /vsong <judul lagu> : Untuk Mendownload Video di YouTube dengan detail
- × /deezer <judul lagu> : Untuk Mendownload lagu dari deezer 
- × /saavn <judul lagu> : Untuk Mendownload lagu dari website saavn
- × /search <judul lagu> : Untuk Mencari Video di YouTube dengan detail
+ × /song [judul lagu] : Untuk Mendownload lagu di YouTube 
+ × /video [judul lagu] : Untuk Mendownload Video di YouTube dengan detail
+ × /vsong [judul lagu] : Untuk Mendownload Video di YouTube dengan detail
+ × /deezer [judul lagu] : Untuk Mendownload lagu dari deezer 
+ × /saavn [judul lagu] : Untuk Mendownload lagu dari website saavn
+ × /search [judul lagu] : Untuk Mencari Video di YouTube dengan detail
 
 PERINTAH UNTUK SEMUA ADMIN GRUP
 
-× /play <judul lagu> : Untuk Memutar lagu yang Anda minta melalui youtube
-× /play <link yt> : Untuk Memutar lagu yang Anda minta melalui link youtube
-× /play <reply ke audio> : Untuk Memutar lagu yang Anda minta melalui file audio
-× /dplay : Untuk Memutar lagu yang Anda minta melalui deezer
-× /splay : Untuk Memutar lagu yang Anda minta melalui jio saavn
+× /play [judul lagu] : Untuk Memutar lagu yang Anda minta melalui youtube
+× /play [link yt] : Untuk Memutar lagu yang Anda minta melalui link youtube
+× /play [reply ke audio] : Untuk Memutar lagu yang Anda minta melalui file audio
+× /dplay [judul lagu] : Untuk Memutar lagu yang Anda minta melalui deezer
+× /splay [judul lagu] : Untuk Memutar lagu yang Anda minta melalui jio saavn
 × /skip : Untuk Menskip pemutaran lagu ke Lagu berikutnya
 × /pause : Untuk Menjeda pemutaran Lagu
 × /resume : Untuk Melanjutkan pemutaran Lagu yang di pause
